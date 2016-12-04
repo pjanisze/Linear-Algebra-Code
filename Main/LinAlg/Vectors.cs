8f729115665ee97e48e94ceceea4b4335d7f29a3
Paul Janiszewski <pauljaniszewski@v1022-rn-171-116.campus-dynamic.uwaterloo.ca>
2016-12-04 01:36:44 -0500

    added vector fucntions

---
+++ Main/LinAlg/Vectors.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LinAlg
+{
+	public class Vectors
+	{
+		public Vectors()
+		{
+		}
+
+
+		public static Vector createVec(int n)
+		{
+			System.Diagnostics.Debug.Assert(n > 0);
+			Vector vec = new Vector();
+			vec.size = n;
+			vec.vars = new double[vec.size];
+			Console.WriteLine("Enter the values of the vector:");
+			for (int i = 0; i < vec.size; i++)
+			{
+				vec.vars[i] = Convert.ToDouble(Console.ReadLine());
+			}
+			return vec;
+
+		}
+
+		public static Vector vectorAdd(Vector v1, Vector v2)
+		{
+			System.Diagnostics.Debug.Assert(v1.size == v2.size);
+			Vector vec = new Vector();
+			vec.size = v1.size;
+			vec.vars = new double[vec.size];
+			for (int i = 0; i < vec.size; i++)
+			{
+				vec.vars[i] = v1.vars[i] + v2.vars[i];
+			}
+
+			return vec;
+		}
+		public static Vector crossProd(Vector v1, Vector v2)
+		{
+			System.Diagnostics.Debug.Assert(v1.size == 3 && v2.size == 3);
+			Vector vec = new Vector();
+			vec.size = 3;
+			vec.vars = new double[3];
+			vec.vars[0] = (v1[1] * v2[2]) - (v1[2] * v2[1]);
+			vec.vars[1] = (v1[2] * v2[0]) - (v1[0] * v2[2]);
+			vec.vars[2] = (v1[0] * v2[1]) - (v1[1] * v2[0]);
+			return vec;
+		}
+		public static double dotProd(Vector v1, Vector v2)
+		{
+			System.Diagnostics.Debug.Assert(v1.size == v2.size);
+			Vector vec = new Vector();
+			vec.size = v1.size;
+			vec.vars = new double[vec.size];
+			double dot = 0.0;
+			for (int i = 0; i < vec.size; i++)
+			{
+				dot += (v1.vars[i] * v2.vars[i]);
+			}
+			return dot;
+		}
+
+	}
+}
---
+++ Main/LinAlg/structs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinAlg
+{
+	
+	public struct Matrix
+	{
+		public double[][] vars;
+		public int rows;
+		public int cols;
+	}
+	public struct Vector
+	{
+		public double[] vars;
+		public int size;
+	}
+}
